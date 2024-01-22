e769c818523c1f18c6b7a5b1e112907349a11766
Przemek Kosakowski (przemosk) <p.kosakowski@gmail.com>
2024-01-22 15:45:27 +0100

    add rubocop, rspec gem, setup, adjust rubocop rules

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# Base application module for helpers
 module ApplicationHelper
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
+# Base application class for mailers
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  default from: 'from@example.com'
+  layout 'mailer'
 end
