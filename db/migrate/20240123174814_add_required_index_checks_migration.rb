e9e76e633011b73b410297648434a101307efa29
Przemek Kosakowski (przemosk) <p.kosakowski@gmail.com>
2024-01-23 20:21:49 +0100

    fix bug in migration

--- db/migrate/20240123174814_add_required_index_checks_migration.rb
+++ db/migrate/20240123174814_add_required_index_checks_migration.rb
@@ -7,6 +7,6 @@ class AddRequiredIndexChecksMigration < ActiveRecord::Migration[7.1]
     remove_column :orders, :amount, :decimal
 
     add_column :merchants, :minimum_monthly_fee, :decimal, precision: 10, scale: 2, default: 0.0, null: false
-    add_column :orders, :amount, precision: 10, scale: 2, default: 0.0, null: false
+    add_column :orders, :amount, :decimal, precision: 10, scale: 2, default: 0.0, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_23_174402) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_23_174814) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -31,14 +31,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_23_174402) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.decimal "minimum_monthly_fee"
+    t.decimal "minimum_monthly_fee", precision: 10, scale: 2, default: "0.0", null: false
   end
 
   create_table "orders", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.decimal "amount", precision: 8, scale: 2, default: "0.0"
     t.uuid "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "amount", precision: 10, scale: 2, default: "0.0", null: false
     t.index ["merchant_id"], name: "index_orders_on_merchant_id"
   end
 
